987215528b1f7f25caedaf37955f794c4f8799b1
Sebastian Neuner <sneuner@google.com>
2024-11-13 15:14:14 +0100

    Re-format origin isolation audit and enable it in the default config.

--- core/audits/origin-isolation.js
+++ core/audits/origin-isolation.js
@@ -5,6 +5,7 @@
  */
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import {MainResource} from '../computed/main-resource.js';
 import * as i18n from '../lib/i18n/i18n.js';
 
@@ -16,23 +17,55 @@ import {MainResource} from '../computed/main-resource.js';
 import * as i18n from '../lib/i18n/i18n.js';
 
 >>>>>>> 287296188 (Add Lighthouse audit to check for presence of the COOP header (origin isolation).)
+=======
+import {MainResource} from '../computed/main-resource.js';
+import * as i18n from '../lib/i18n/i18n.js';
+
+import {Audit} from './audit.js';
+
+>>>>>>> 4d7aa600b (Re-format origin isolation audit and enable it in the default config.)
 const UIStrings = {
-  /** Title of a Lighthouse audit that evaluates the security of a page's COOP header for origin isolation. "COOP" stands for "Cross-Origin-Opener-Policy". */
+  /**
+     Title of a Lighthouse audit that evaluates the security of a page's COOP
+     header for origin isolation. "COOP" stands for
+     "Cross-Origin-Opener-Policy".
+   */
   title: 'Ensure the proper usage of the COOP header to isolate the origin.',
-  /** Description of a Lighthouse audit that evaluates the security of a page's COOP header for origin isolation. This is displayed after a user expands the section to see more. No character length limits. The last sentence starting with 'Learn' becomes link text to additional documentation. "COOP" stands for "Cross-Origin-Opener-Policy". */
+  /**
+     Description of a Lighthouse audit that evaluates the security of a page's
+     COOP header for origin isolation. This is displayed after a user expands
+     the section to see more. No character length limits. The last sentence
+     starting with 'Learn' becomes link text to additional documentation. "COOP"
+     stands for "Cross-Origin-Opener-Policy".
+   */
   description: 'Deployment of the COOP header allows isolation of the top-level document to not share a browsing context group with cross-origin documents. ' +
+<<<<<<< HEAD
 <<<<<<< HEAD
     '[Learn what the COOP header is and how it should be deployed.](https://web.dev/articles/why-coop-coep#coop)',
 =======
     '[Learn what the COOP header is and how it should be deployed.](https://link-to-background)',
 >>>>>>> 287296188 (Add Lighthouse audit to check for presence of the COOP header (origin isolation).)
   /** Summary text for the results of a Lighthouse audit that evaluates the COOP header for origin isolation. This is displayed if no COOP header is deployed. "COOP" stands for "Cross-Origin-Opener-Policy". */
+=======
+      '[Learn what the COOP header is and how it should be deployed.](https://link-to-background)',
+  /**
+     Summary text for the results of a Lighthouse audit that evaluates the COOP
+     header for origin isolation. This is displayed if no COOP header is
+     deployed. "COOP" stands for "Cross-Origin-Opener-Policy".
+   */
+>>>>>>> 4d7aa600b (Re-format origin isolation audit and enable it in the default config.)
   noCoop: 'No COOP header found',
   /** Table item value calling out the presence of a syntax error. */
   invalidSyntax: 'Invalid syntax',
-  /** Label for a column in a data table; entries will be a directive of the COOP header. "COOP" stands for "Cross-Origin-Opener-Policy". */
+  /**
+     Label for a column in a data table; entries will be a directive of the
+     COOP header. "COOP" stands for "Cross-Origin-Opener-Policy".
+   */
   columnDirective: 'Directive',
-  /** Label for a column in a data table; entries will be the severity of an issue with the COOP header. "COOP" stands for "Cross-Origin-Opener-Policy". */
+  /**
+     Label for a column in a data table; entries will be the severity of an
+     issue with the COOP header. "COOP" stands for "Cross-Origin-Opener-Policy".
+   */
   columnSeverity: 'Severity',
 };
 
@@ -80,16 +113,18 @@ class OriginIsolation extends Audit {
    */
   static async getRawCoop(artifacts, context) {
     const devtoolsLog = artifacts.devtoolsLogs[Audit.DEFAULT_PASS];
-    const mainResource = await MainResource.request({devtoolsLog, URL: artifacts.URL}, context);
+    const mainResource =
+        await MainResource.request({devtoolsLog, URL: artifacts.URL}, context);
 
-    var coopHeaders = mainResource.responseHeaders
-      .filter(h => {
-        return h.name.toLowerCase() === 'cross-origin-opener-policy';
-      })
-      .flatMap(h => h.value);
+    var coopHeaders =
+        mainResource.responseHeaders
+            .filter(h => {
+              return h.name.toLowerCase() === 'cross-origin-opener-policy';
+            })
+            .flatMap(h => h.value);
 
-      // Sanitize the header value.
-      coopHeaders = coopHeaders.map(v => v.toLowerCase().replace(/\s/g, ''));
+    // Sanitize the header value.
+    coopHeaders = coopHeaders.map(v => v.toLowerCase().replace(/\s/g, ''));
 
     return {coopHeaders};
 >>>>>>> 287296188 (Add Lighthouse audit to check for presence of the COOP header (origin isolation).)
@@ -116,11 +151,16 @@ class OriginIsolation extends Audit {
   static constructResults(coopHeaders) {
     const rawCoop = [...coopHeaders];
 <<<<<<< HEAD
+<<<<<<< HEAD
     const allowedDirectives =
         ['unsafe-none', 'same-origin-allow-popups', 'same-origin'];
 =======
     const allowedDirectives = [ 'unsafe-none', 'same-origin-allow-popups', 'same-origin' ];
 >>>>>>> 287296188 (Add Lighthouse audit to check for presence of the COOP header (origin isolation).)
+=======
+    const allowedDirectives =
+        ['unsafe-none', 'same-origin-allow-popups', 'same-origin'];
+>>>>>>> 4d7aa600b (Re-format origin isolation audit and enable it in the default config.)
     const violations = [];
     const syntax = [];
 
@@ -139,6 +179,7 @@ class OriginIsolation extends Audit {
     for (const actualDirective of coopHeaders) {
       // If there is a directive that's not an official COOP directive.
 <<<<<<< HEAD
+<<<<<<< HEAD
       if (!allowedDirectives.includes(actualDirective)) {
         syntax.push({
           severity: str_(i18n.UIStrings.itemSeverityLow),
@@ -147,6 +188,9 @@ class OriginIsolation extends Audit {
         });
 =======
       if(!allowedDirectives.includes(actualDirective)){
+=======
+      if (!allowedDirectives.includes(actualDirective)) {
+>>>>>>> 4d7aa600b (Re-format origin isolation audit and enable it in the default config.)
         syntax.push({
           severity: str_(i18n.UIStrings.itemSeverityLow),
           description: str_(UIStrings.invalidSyntax),
@@ -159,6 +203,7 @@ class OriginIsolation extends Audit {
     const results = [
       ...violations.map(
 <<<<<<< HEAD
+<<<<<<< HEAD
           f => this.findingToTableItem(
               f.directive, f.description,
               str_(i18n.UIStrings.itemSeverityHigh))),
@@ -167,6 +212,11 @@ class OriginIsolation extends Audit {
             f.directive, f.description,
             str_(i18n.UIStrings.itemSeverityHigh))),
 >>>>>>> 287296188 (Add Lighthouse audit to check for presence of the COOP header (origin isolation).)
+=======
+          f => this.findingToTableItem(
+              f.directive, f.description,
+              str_(i18n.UIStrings.itemSeverityHigh))),
+>>>>>>> 4d7aa600b (Re-format origin isolation audit and enable it in the default config.)
       ...syntax.map(
           f => this.findingToTableItem(
               f.directive, f.description,
@@ -196,9 +246,24 @@ class OriginIsolation extends Audit {
     /** @type {LH.Audit.Details.Table['headings']} */
     const headings = [
       /* eslint-disable max-len */
-      {key: 'description', valueType: 'text', subItemsHeading: {key: 'description'}, label: str_(i18n.UIStrings.columnDescription)},
-      {key: 'directive', valueType: 'code', subItemsHeading: {key: 'directive'}, label: str_(UIStrings.columnDirective)},
-      {key: 'severity', valueType: 'text', subItemsHeading: {key: 'severity'}, label: str_(UIStrings.columnSeverity)},
+      {
+        key: 'description',
+        valueType: 'text',
+        subItemsHeading: {key: 'description'},
+        label: str_(i18n.UIStrings.columnDescription)
+      },
+      {
+        key: 'directive',
+        valueType: 'code',
+        subItemsHeading: {key: 'directive'},
+        label: str_(UIStrings.columnDirective)
+      },
+      {
+        key: 'severity',
+        valueType: 'text',
+        subItemsHeading: {key: 'severity'},
+        label: str_(UIStrings.columnSeverity)
+      },
       /* eslint-enable max-len */
     ];
     const details = Audit.makeTableDetails(headings, results);
