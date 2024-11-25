5bbb40b42de4433246d997b5c9ba4cdd0eddbd16
Sebastian Neuner <sebastian9er@users.noreply.github.com>
2024-11-25 11:30:05 +0100

    Update core/audits/has-hsts.js
    Co-authored-by: Adam Raine <6752989+adamraine@users.noreply.github.com>

--- core/audits/has-hsts.js
+++ core/audits/has-hsts.js
@@ -25,7 +25,7 @@ const UIStrings = {
   /** Summary text for the results of a Lighthouse audit that evaluates the HSTS header. This is displayed if the max-age directive is missing. "HSTS" stands for "HTTP Strict Transport Security". */
   noMaxAge: 'No `max-age` directive',
   /** Summary text for the results of a Lighthouse audit that evaluates the HSTS header. This is displayed if the provided duration for the max-age directive is too low. "HSTS" stands for "HTTP Strict Transport Security". */
-  lowMaxAge: 'max-age is too low',
+  lowMaxAge: '`max-age` is too low',
   /** Table item value calling out the presence of a syntax error. */
   invalidSyntax: 'Invalid syntax',
   /** Label for a column in a data table; entries will be a directive of the HSTS header. "HSTS" stands for "HTTP Strict Transport Security". */
